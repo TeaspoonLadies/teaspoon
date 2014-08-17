76a90973458f3729a04687075de5525510af60e6
github username <github email address>
2014-08-17 11:18:25 -0400

    trying to print converted recipe but not working

--- app/assets/javascripts/form_page.js
+++ app/assets/javascripts/form_page.js
@@ -1,13 +1,14 @@
-// $("#submit_button").click(function(e) {
-// 	console.log("button clicked");
-// 	alert("hi");
-// 	e.preventDefault();
-// });
-
-
+$('#recipe_converter_form').submit(function() {
+	console.log("button clicked");
+	alert("hi");
+	$("#results").change(function(e) {
+		$("#results").text(Recipe.conversion(string));
+	});
+});
 
 $(document).on('#submit_button', '#recipe_converter_form', function(e) {
 	$.ajax({
+		type: 'GET',
 		url: $(this).attr('action'),
 		type: $(this).attr('method'),
 		data: $(this).serialize(),

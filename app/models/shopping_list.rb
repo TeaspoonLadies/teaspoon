339920aa73db16c6ae42445e066b64a6f1696658
Denine Guy <denineguy@gmail.com>
2014-08-21 18:11:27 -0400

    Able to render the grocery list view

--- app/models/shopping_list.rb
+++ app/models/shopping_list.rb
@@ -1,15 +1,15 @@
 class ShoppingList < ActiveRecord::Base
   # belongs_to :user
-  before_save :grocery_list
   belongs_to :recipe
 
   def grocery_list
     text = self.recipe.ingredients
+    name = self.recipe.name
     binding.pry
     text.gsub!("\r\n", '</li><li><en-todo checked="false"/>')
     text.slice!(0..4)
     text << "</li>"
-    text.insert(0, "<en-note><h1>#{recipe}</h1><ul>")
+    text.insert(0, "<en-note><h1>"+ name +"</h1><ul>")
     text << "</ul></en-note>"
     binding.pry
     self.content = text

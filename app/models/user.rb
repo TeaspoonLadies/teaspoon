bc0eeee53b15b8b79cfc912a7d8f5ad07f2b084d
github username <github email address>
2014-08-25 22:53:15 -0400

    changed denine's picture

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@ class User < ActiveRecord::Base
   def self.create_with_omniauth(auth_hash)
     # self.create(:provider = auth_hash["provider"], :name = auth_hash["info"]["name"], :uid = auth_hash["uid"])
     create do |user|
-      binding.pry
       user.provider = auth_hash["provider"]
       user.uid = auth_hash["uid"]
       user.name = auth_hash["username"]
